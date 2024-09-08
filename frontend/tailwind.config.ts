7605df6e43f455ffd9887b94aab7af969283e24b
aayushker <singhaayushker@gmail.com>
2024-09-08 12:44:23 +0530

    Added the dependdency

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,19 +1,29 @@
 import type { Config } from "tailwindcss";
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate"), nextui()],
 };
 export default config;
