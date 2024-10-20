a4f5d14d4e7f90901b8aefda367ed3a27dd6555d
aayushker <singhaayushker@gmail.com>
2024-10-20 21:56:13 +0530

    Updated index files

---
+++ frontend/pages/book/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import BookTicket from "@/app/sections/bookTicket/BookTicket";
+import "@/app/globals.css";
+
+const index = () => {
+  return (
+    <>
+      <BookTicket />
+    </>
+  );
+};
+
+export default index;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,19 +3,22 @@ import LandingPage from "@/app/sections/Landing/Hero";
 import NavBar from "@/app/components/NavBar";
 import Footer from "@/app/components/Footer";
 import "@/app/globals.css";
+import { AuthProvider } from "@/app/AuthContext";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-white shadow-md">
-        <NavBar />
-      </header>
-      <main className="">
-        <LandingPage />
-      </main>
-      <footer className="bg-gray-100 py-8">
-        <Footer />
-      </footer>
-    </div>
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col">
+        <header className="bg-white shadow-md">
+          <NavBar />
+        </header>
+        <main className="">
+          <LandingPage />
+        </main>
+        <footer className="bg-gray-100 py-8">
+          <Footer />
+        </footer>
+      </div>
+    </AuthProvider>
   );
 }
